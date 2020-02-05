b0bcb7888178ab18a725481c7d49fa15ca3e3a64
Nikola <nikola@gearsaid.com>
2020-02-05 15:22:38 +0100

    Changed home page a bit and added button to trigger subscription

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { subscribeUser } from './subscription';
 import './App.css';
 
 function App() {
@@ -10,14 +11,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+            Push notifications demo.
+          </p>
+          <p>Subscribe to notification servrvice and recive push notification</p>
+          <button onClick={subscribeUser}>Subscribe</button>
       </header>
     </div>
   );
