34c464c59ad4ff72191b3047d7bd8f0d8eedc56b
Nikola <marinko.magla91@gmail.com>
2020-02-07 13:18:11 +0100

    populate home component and add it in routing

--- src/App.js
+++ src/App.js
@@ -1,57 +1,16 @@
 import React from "react";
-import {
-  subscribeUser,
-  getNotification,
-  getDelayedNotification
-} from "./subscription";
-import Logo from "./images/logo.png";
 import "./App.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faExchangeAlt } from "@fortawesome/free-solid-svg-icons";
-import { faBell } from "@fortawesome/free-solid-svg-icons";
-import { faHourglassStart } from "@fortawesome/free-solid-svg-icons";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <div className="App">
-            <div className="navigation">
-              <img src={Logo} className="logo" alt="logo" />
-              <h1>BLApp Push Notification Demo</h1>
-            </div>
-
-            <div className="button-grp">
-              <div className="button-wrap">
-                <p>Subscribe to our service to receive notifications</p>
-                <button onClick={subscribeUser}>
-                  <FontAwesomeIcon icon={faExchangeAlt} color="white" />
-                </button>
-              </div>
-
-              <div className="button-wrap">
-                <p>Receive Notification</p>
-                <button onClick={getNotification}>
-                  <FontAwesomeIcon icon={faBell} color="white" />
-                </button>
-              </div>
-              <div className="button-wrap">
-                <p>Receive delayed notification (60sec)</p>
-                <button onClick={getDelayedNotification}>
-                  <FontAwesomeIcon icon={faHourglassStart} color="white" />
-                </button>
-              </div>
-            </div>
-
-            <div className="background"></div>
-            <div className="mask"></div>
-          </div>
+          <Home />
         </Route>
-        <Route path="/event">
-          <p>TEST</p>
-        </Route>
+        <Route path="/event"></Route>
       </Switch>
     </Router>
   );
---
+++ src/Home.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  subscribeUser,
+  getNotification,
+  getDelayedNotification
+} from "./subscription";
+import Logo from "./images/logo.png";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExchangeAlt } from "@fortawesome/free-solid-svg-icons";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
+import { faHourglassStart } from "@fortawesome/free-solid-svg-icons";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="navigation">
+        <img src={Logo} className="logo" alt="logo" />
+        <h1>BLApp Push Notification Demo</h1>
+      </div>
+
+      <div className="button-grp">
+        <div className="button-wrap">
+          <p>Subscribe to our service to receive notifications</p>
+          <button onClick={subscribeUser}>
+            <FontAwesomeIcon icon={faExchangeAlt} color="white" />
+          </button>
+        </div>
+
+        <div className="button-wrap">
+          <p>Receive Notification</p>
+          <button onClick={getNotification}>
+            <FontAwesomeIcon icon={faBell} color="white" />
+          </button>
+        </div>
+        <div className="button-wrap">
+          <p>Receive delayed notification (60sec)</p>
+          <button onClick={getDelayedNotification}>
+            <FontAwesomeIcon icon={faHourglassStart} color="white" />
+          </button>
+        </div>
+      </div>
+
+      <div className="background"></div>
+      <div className="mask"></div>
+    </div>
+  );
+}
