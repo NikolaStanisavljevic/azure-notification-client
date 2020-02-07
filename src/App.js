f9b5e4f0e8864596e3a6ff0184a9b5db52392e17
Nikola <marinko.magla91@gmail.com>
2020-02-07 11:23:38 +0100

    Added font-awsome icons to buttons, added new button for delayd notification

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from 'react';
-import { subscribeUser, getNotification } from './subscription';
+import React from "react";
+import {
+  subscribeUser,
+  getNotification,
+  getDelayedNotification
+} from "./subscription";
 import Logo from "./images/logo.png";
-import './App.css';
-
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExchangeAlt } from "@fortawesome/free-solid-svg-icons";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
+import { faHourglassStart } from "@fortawesome/free-solid-svg-icons";
 function App() {
   return (
     <div className="App">
-          <img src={Logo}/>
-          <h1>BLApp</h1>
-          <p>BLApp</p>
-          <button onClick={subscribeUser}>Subscribe</button>
-          <button onClick={getNotification}>Send Notification</button>
-          <div className="background"></div>
-          <div className="mask"></div>
+      <div className="navigation">
+        <img src={Logo} className="logo" />
+        <h1>BLApp Push Notification Demo</h1>
+      </div>
+
+      <button onClick={subscribeUser}>
+        <FontAwesomeIcon icon={faExchangeAlt} color="white" />
+        Subscribe
+      </button>
+      <button onClick={getNotification}>
+        <FontAwesomeIcon icon={faBell} color="white" />
+        Send Notification
+      </button>
+
+      <button onClick={getDelayedNotification}>
+        <FontAwesomeIcon icon={faHourglassStart} color="white" />
+        Send Delayed Notification
+      </button>
+      <div className="background"></div>
+      <div className="mask"></div>
     </div>
   );
 }
