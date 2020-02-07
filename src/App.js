ba6861e160323b24ff07a60f5f39e67de044185a
Nikola <marinko.magla91@gmail.com>
2020-02-07 13:15:53 +0100

    Install react router and create separate components for Home and Event

--- src/App.js
+++ src/App.js
@@ -10,40 +10,50 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExchangeAlt } from "@fortawesome/free-solid-svg-icons";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
 import { faHourglassStart } from "@fortawesome/free-solid-svg-icons";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <div className="navigation">
-        <img src={Logo} className="logo" alt="logo" />
-        <h1>BLApp Push Notification Demo</h1>
-      </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <div className="App">
+            <div className="navigation">
+              <img src={Logo} className="logo" alt="logo" />
+              <h1>BLApp Push Notification Demo</h1>
+            </div>
 
-      <div className="button-grp">
-        <div className="button-wrap">
-          <p>Subscribe to our service to receive notifications</p>
-          <button onClick={subscribeUser}>
-            <FontAwesomeIcon icon={faExchangeAlt} color="white" />
-          </button>
-        </div>
+            <div className="button-grp">
+              <div className="button-wrap">
+                <p>Subscribe to our service to receive notifications</p>
+                <button onClick={subscribeUser}>
+                  <FontAwesomeIcon icon={faExchangeAlt} color="white" />
+                </button>
+              </div>
 
-        <div className="button-wrap">
-          <p>Receive Notification</p>
-          <button onClick={getNotification}>
-            <FontAwesomeIcon icon={faBell} color="white" />
-          </button>
-        </div>
-        <div className="button-wrap">
-          <p>Receive delayed notification (60sec)</p>
-          <button onClick={getDelayedNotification}>
-            <FontAwesomeIcon icon={faHourglassStart} color="white" />
-          </button>
-        </div>
-      </div>
+              <div className="button-wrap">
+                <p>Receive Notification</p>
+                <button onClick={getNotification}>
+                  <FontAwesomeIcon icon={faBell} color="white" />
+                </button>
+              </div>
+              <div className="button-wrap">
+                <p>Receive delayed notification (60sec)</p>
+                <button onClick={getDelayedNotification}>
+                  <FontAwesomeIcon icon={faHourglassStart} color="white" />
+                </button>
+              </div>
+            </div>
 
-      <div className="background"></div>
-      <div className="mask"></div>
-    </div>
+            <div className="background"></div>
+            <div className="mask"></div>
+          </div>
+        </Route>
+        <Route path="/event">
+          <p>TEST</p>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
