b152b1f5c77d0178dd40233a07484ec8cf8d55de
Nikola <marinko.magla91@gmail.com>
2020-02-07 13:45:13 +0100

    Finish event page

--- src/Event.js
+++ src/Event.js
@@ -1,9 +1,14 @@
 import React from "react";
+import EventPhoto from "./images/event.png";
+import Logo from "./images/logo.png";
 
 function Event() {
   return (
-    <div>
-      <h4>Event page</h4>
+    <div className="event-page">
+      <h4>Welcome to</h4>
+      <img src={EventPhoto} alt="event" className="event-img" />
+      <h4>Our new event...</h4>
+      <img src={Logo} alt="event" className="event-logo" />
     </div>
   );
 }
