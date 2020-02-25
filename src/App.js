7e6fee22cd6f7c3333d094a0f7ff61de2b0e151d
Nikola <marinko.magla91@gmail.com>
2020-02-25 13:17:26 +0100

    Added ionic as build wrapper and implemented OneSignal notification via cdn

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "@ionic/react/css/core.css";
+/* Basic CSS for apps built with Ionic */
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+
+/* Optional CSS utils that can be commented out */
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+
+import { Route } from "react-router-dom";
+import { IonApp, IonRouterOutlet } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
 import Home from "./Home";
 import Event from "./Event";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/event">
-          <Event />
-        </Route>
-      </Switch>
-    </Router>
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route path="/" exact component={Home} />
+          <Route path="/event" exact component={Event} />
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
   );
 }
 
