724d0fe7f543fff9369c5f5ff0a46a1954325c73
Nikola <marinko.magla91@gmail.com>
2020-02-28 17:23:20 +0100

    Changes related to reciving push notifications on ios via OneSignal service

--- src/App.js
+++ src/App.js
@@ -19,8 +19,38 @@ import { IonApp, IonRouterOutlet } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
 import Home from "./Home";
 import Event from "./Event";
+import { OneSignal } from "@ionic-native/onesignal/ngx";
 
-function App() {
+const App = () => {
+  const initNotifications = () => {
+    OneSignal.startInit("9753bb3a-52fb-472a-abd8-3eeef79de1d3", "703322744261");
+
+    OneSignal.inFocusDisplaying(OneSignal.OSInFocusDisplayOption.InAppAlert);
+
+    OneSignal.handleNotificationReceived().subscribe(data => {
+      // do something when notification is received
+      let msg = data.payload.body;
+      let title = data.payload.title;
+      let additionalData = data.payload.additionalData;
+      this.showAlert(title, msg, additionalData.task);
+    });
+
+    OneSignal.handleNotificationOpened().subscribe(data => {
+      // do something when a notification is opened
+      let additionalData = data.notification.payload.additionalData;
+
+      showAlert(
+        "Notification opened",
+        "You already read this before",
+        additionalData.task
+      );
+    });
+
+    this.oneSignal.endInit();
+  };
+
+  initNotifications();
+
   return (
     <IonApp>
       <IonReactRouter>
@@ -31,6 +61,22 @@ function App() {
       </IonReactRouter>
     </IonApp>
   );
+};
+
+async function showAlert(title, msg, task) {
+  const alert = await this.alertCtrl.create({
+    header: title,
+    subHeader: msg,
+    buttons: [
+      {
+        text: `Action: ${task}`,
+        handler: () => {
+          // E.g: Navigate to a specific screen
+        }
+      }
+    ]
+  });
+  alert.present();
 }
 
 export default App;
