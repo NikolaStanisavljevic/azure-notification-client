46d7a9c399e2acc78720f7c4431df0ddb0224540
Nikola <marinko.magla91@gmail.com>
2020-03-18 20:31:49 +0100

    Set new buttons on home page, create function to transfer to users page on button click

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,14 @@
 import React from "react";
-import {
-  subscribeUser,
-  getNotification,
-  getDelayedNotification
-} from "./subscription";
+
 import Logo from "./images/logo.png";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExchangeAlt } from "@fortawesome/free-solid-svg-icons";
-import { faBell } from "@fortawesome/free-solid-svg-icons";
-import { faHourglassStart } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
+  const showUsersPage = () => {
+    window.location.href = "/users";
+  };
   return (
     <>
       <div className="navigation">
@@ -21,22 +18,9 @@ export default function Home() {
 
       <div className="button-grp">
         <div className="button-wrap">
-          <p>Subscribe to our service to receive notifications</p>
-          <button onClick={subscribeUser}>
+          <p>View list of our test users</p>
+          <button onClick={showUsersPage}>
             <FontAwesomeIcon icon={faExchangeAlt} color="white" />
-          </button>
-        </div>
-
-        <div className="button-wrap">
-          <p>Receive Notification</p>
-          <button onClick={getNotification}>
-            <FontAwesomeIcon icon={faBell} color="white" />
-          </button>
-        </div>
-        <div className="button-wrap">
-          <p>Receive delayed notification (60sec)</p>
-          <button onClick={getDelayedNotification}>
-            <FontAwesomeIcon icon={faHourglassStart} color="white" />
           </button>
         </div>
       </div>
